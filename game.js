575d00b8e34cec7b1479ada1895f053dd26245b3
Anton <ovsyannikov.a@yahoo.com>
2021-05-05 18:23:59 +0200

    change movement
    changed movement to 'wasd'

--- game.js
+++ game.js
@@ -57,7 +57,7 @@ function enemyCreator(type,x,y,scene){
 
     if(type==1){
         var concreteEnemy= scene.physics.add.sprite(x, y, 'demon').setData({ hp: 70, music: '120_ADNDA', speed:350, pattern: [30,540,1040,1535,2010],satrnumber: 5,pause:2500 ,attacks: [0,1,2,1,0]});
-        concreteEnemy.anims.play('demon_idle',true);
+        concreteEnemy.anims.play('demon_idle',true); // TO DO: fix animation or delete it
     }
     if(type==2) var concreteEnemy= scene.physics.add.sprite(x, y, 'star').setData({ hp: 40, music: '180_ADNDDANDDA', speed:300, pattern: [28,210,365,540,710,855,1030,1190,1370,1530],satrnumber: 10,pause:2000 ,attacks: [0,1,2,1,1,0,2,1,1,0]});
     enemy.add(concreteEnemy);
@@ -127,12 +127,12 @@ function create ()
 
 function update ()
 {
-    movement(cursors,player,battle,lose);
+    movement(cursors,player,battle,lose,this);
     if(lose==1){
         PlayerHPText.setText('GAME OVER' );
         
     }
-    
+
     
 }
 function dropStar(music,enemyinfo,startype,keyA,keyD){
