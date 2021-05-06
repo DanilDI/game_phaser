9b1430fa8eeb7183cbb3dddb8add3d907af2514b
Anton <ovsyannikov.a@yahoo.com>
2021-05-06 15:10:06 +0200

    new enemies anims
    dungen and forerst enemies

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ var enemy;
 var player;
 var stars;
 var platforms;
+
 var battle=0;
 var lose=0;
 var EnemyHPText;
@@ -54,11 +55,25 @@ function preload ()
     this.load.spritesheet('run_right', 'assets/main_char_anims/run.png', { frameWidth: 184, frameHeight: 137 });
     this.load.spritesheet('idle', 'assets/main_char_anims/idle.png', { frameWidth: 184, frameHeight: 137 });
     this.load.spritesheet('run_left', 'assets/main_char_anims/run_left.png', { frameWidth: 184, frameHeight: 137 });
+    //hell
     this.load.spritesheet('demon', 'assets/hell_anims/demon-idle.png', { frameWidth: 160, frameHeight: 144 });
     this.load.spritesheet('ghost', 'assets/hell_anims/ghost-idle.png', { frameWidth: 64, frameHeight: 80 });
     this.load.spritesheet('hell-beast', 'assets/hell_anims/hell-beast-idle.png', { frameWidth: 55, frameHeight: 67 });
     this.load.spritesheet('hell-hound', 'assets/hell_anims/hell-hound-idle.png', { frameWidth: 64, frameHeight: 32 });
     this.load.spritesheet('nightmare', 'assets/hell_anims/nightmare-idle.png', { frameWidth: 128, frameHeight: 96 });
+    //dungeon
+    this.load.spritesheet('skeleton', 'assets/dungeon_anims/skeleton-idle.png', { frameWidth: 150, frameHeight: 150 });
+    this.load.spritesheet('goblin', 'assets/dungeon_anims/goblin-idle.png', { frameWidth: 150, frameHeight: 150 });
+    this.load.spritesheet('eye', 'assets/dungeon_anims/eye-idle.png', { frameWidth: 150, frameHeight: 150 });
+    this.load.spritesheet('zombie', 'assets/dungeon_anims/zombie-idle.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('bat', 'assets/dungeon_anims/bat-idle.png', { frameWidth: 16, frameHeight: 24 }); //setScale(1.5)
+    //forest 
+    this.load.spritesheet('mushroom', 'assets/forest_anims/mushroom-idle.png', { frameWidth: 150, frameHeight: 150 });
+    this.load.spritesheet('huntress', 'assets/forest_anims/huntress-idle.png', { frameWidth: 150, frameHeight: 150 });
+    this.load.spritesheet('cyclop', 'assets/forest_anims/cyclop-idle.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('worm', 'assets/forest_anims/worm-idle.png', { frameWidth: 64, frameHeight: 64 });
+
+    this.load.image('invibcible_shield', 'assets/items/invibcible_shield.png');
 
 }
 function enemyCreator(type,x,y,scene){
@@ -69,20 +84,25 @@ function enemyCreator(type,x,y,scene){
     if(type==3) var concreteEnemy= scene.physics.add.sprite(x, y, 'ghost').setData({animation:'ghost-idle', hp: 50, music: '120_DDDAAAND', speed:300, pattern: [35,535,1035,1510,1770,2020,2270,2510],satrnumber: 8,pause:3000 ,attacks: [1,1,1,0,0,0,2,1]});
     if(type==4) var concreteEnemy= scene.physics.add.sprite(x, y, 'hell-beast').setData({animation:'hell-beast-idle', hp: 80, music: '60_AAAADNDD', speed:300, pattern: [30,770,1270,1525,1780,2020,2215,2530],satrnumber: 8,pause:3000 ,attacks: [0,0,0,0,1,2,1,1]});
     if(type==5) var concreteEnemy= scene.physics.add.sprite(x, y, 'hell-hound').setData({animation:'hell-hound-idle', hp: 60, music: '150_AADDA', speed:330, pattern: [10,110,215,515,810],satrnumber: 5,pause:1500 ,attacks: [0,0,1,1,0]});
+    
+    if(type==6) var concreteEnemy= scene.physics.add.sprite(x, y, 'worm').setData({animation:'worm-idle', hp: 60, music: '150_AADDA', speed:330, pattern: [10,110,215,515,810],satrnumber: 5,pause:1500 ,attacks: [0,0,1,1,0]});
+    //just 4 test ^
     enemy.add(concreteEnemy);
 }
 function create ()
 {
-    ///карта и окружение
+     ///карта и окружение
     var img = this.add.image(600, 450, 'background');
 	const map = this.make.tilemap({key: 'map'})
 	const tileset = map.addTilesetImage('tileset_dungeon', 'tiles', 32, 32, 0, 0);
 	const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
-
+    
 
     //худ и платформа для звёзд
     mHood = this.physics.add.staticGroup();
     mHood.create(1050,450,'hood').refreshBody();
+    
+    var shield = this.add.image(600, 450, 'invibcible_shield');
     
     platforms = this.physics.add.staticGroup();
     platforms.create(1050, 881, 'ground').setScale(0.71).refreshBody();
@@ -90,22 +110,22 @@ function create ()
     //текст
     EnemyHPText = this.add.text(930, 16, 'Enemy HP:  0', { fontSize: '32px', fill: '#000' });
     PlayerHPText = this.add.text(930, 50, 'Your HP:  100', { fontSize: '32px', fill: '#000' });
-
+    
     //игрок
     player = this.physics.add.sprite(100, 450, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-    
+
     //создание анимаций
     createAnims(this);
     
-    
+   
     //создание физической группы для звёзд
     stars = this.physics.add.group();
 
     //создание физической группы для противников
     enemy = this.physics.add.group();
-    
+
 
     //создание физической группы для предметов
     items = this.physics.add.group();
@@ -116,6 +136,8 @@ function create ()
     enemyCreator(3,400,650,this);
     enemyCreator(4,600,650,this);
     enemyCreator(5,400,400,this);
+
+    enemyCreator(6,400,100,this);//test
 
     //коллайдеры и оверлапы
     this.physics.add.collider(player, mHood);
--- utils/anims.mjs
+++ utils/anims.mjs
@@ -56,4 +56,70 @@ export default function createAnims(focus){
     });
 
     //dungeon
+    focus.anims.create({
+        key: 'skeleton-idle',
+        frames: focus.anims.generateFrameNumbers('skeleton', { start: 0, end: 4 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'goblin-idle',
+        frames: focus.anims.generateFrameNumbers('goblin', { start: 0, end: 4 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'eye-idle',
+        frames: focus.anims.generateFrameNumbers('eye', { start: 0, end: 4 }),
+        frameRate: 7,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'zombie-idle',
+        frames: focus.anims.generateFrameNumbers('zombie', { start: 39, end: 42 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'bat-idle',
+        frames: focus.anims.generateFrameNumbers('bat', { start: 0, end: 5 }),
+        frameRate: 7,
+        repeat: 1
+    });
+
+    //forest
+    focus.anims.create({
+        key: 'mushroom-idle',
+        frames: focus.anims.generateFrameNumbers('mushroom', { start: 0, end: 4 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'huntress-idle',
+        frames: focus.anims.generateFrameNumbers('huntress', { start: 0, end: 7 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'cyclop-idle',
+        frames: focus.anims.generateFrameNumbers('cyclop', { start: 0, end: 14 }),
+        frameRate: 5,
+        repeat: 1
+    });
+
+    focus.anims.create({
+        key: 'worm-idle',
+        frames: focus.anims.generateFrameNumbers('worm', { start: 0, end: 9 }),
+        frameRate: 5,
+        repeat: 1
+    });
+    
+
+
 }
